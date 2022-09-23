ac5d85e3af85109e60a2b335b24c50745312bbb9
lukebrogee <113709371+lukebrogee@users.noreply.github.com>
2022-09-23 12:54:15 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int numberOfClasses = 5;
+        int daysOfClass = 2;
+        int students = 56;
+        int meals = 4;
+        int miles = 10;
+        int days = 100;
+        int weeks = 25;
+        int totalAmountOfClasses;
+
+        double mealPrice = 3.5;
+        double dollars = 2.7;
+        double lunchMoney = 3.6;
+        double savings = 14.8;
+        double checking = 23.9;
+        double total;
+
+        total = savings + checking + dollars;
+        totalAmountOfClasses = numberOfClasses + daysOfClass + weeks;
+
+        System.out.println("The total number of classes I have left is " + totalAmountOfClasses);
+        System.out.println("The amount of money I have total is " + total + " :(");
+    }
+}
